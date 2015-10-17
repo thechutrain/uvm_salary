3eac1904c91a88d64dff3d7837607d3e0c81b46d
thechutrain <chu.chuyalan@gmail.com>
2015-10-17 14:49:00 -0400

    working process on openfile.py

--- openfile.py
+++ openfile.py
@@ -9,10 +9,28 @@ __author__ = 'alanchu'
 # print os.getcwd()
 
 ############################# IMPORT #############################
-import pprint
+import pprint       #to print things pretty
+import csv          #to read csv files
 
-def openfile(filename):
-    try:
-        f = open(filenmae, 'rU')
+def open_cleanfile(filename):
+    #f = open(filename, 'rU')
+    try: #see if you can open file/if file exists
+        f = open(filename, 'rU')
+        reader = csv.reader(f)
+        e = [] #temporary employee row, holds employee data
+        try:
+            for row in reader: #each row is a 3-element list
+                if (row[1]==""):
+                    print row[1]
+                else:
+                    print row
+                    print type(row)
+                    break
+        finally: #assumes you opened the finally, b/c no IOError, so close file!
+            f.close()
     except IOError:
-        
+        print "file not found - please check if the file exists"
+
+##### TESTING ######
+# call function
+open_cleanfile("uvm_employee_salary_project/uvmSalary15.csv") #getting error, b/c this file isn't in uvmemployee_salary_project
