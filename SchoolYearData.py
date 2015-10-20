a2f2fbcc28853f963ad55ab8c288a3d7b4f713d8
thechutrain <chu.chuyalan@gmail.com>
2015-10-20 17:02:49 -0400

    added boxPlotAllEmployees function that makes a box and whisker plot of all the salaries of all employees

--- SchoolYearData.py
+++ SchoolYearData.py
@@ -55,6 +55,42 @@ class SchoolYearData:
             # self.dict_Position_listSalaries[position + year] = temp_list_salaries  # to specify year
         return self.dict_Position_listSalaries
 
+
+    def makeBoxPlot(self):
+        dict_of_pos_vs_salaryList = self.getAllSalariesPosition()
+        for x in dict_of_pos_vs_salaryList:
+            print "this is the box plot for: ", x
+            data = dict_of_pos_vs_salaryList[x]
+            plt.boxplot(data)
+            plt.ylabel("Salary (USD)")
+            plt.show()
+            break
+
+    def boxPlotAllEmployees(self):
+        employee_list = self.employeeMasterList
+        employee_total_salary = []  # list of all the salaries of all employees
+        for e in employee_list:
+            if (e[2] != None):   #  this excludes employees with unpaid leave etc.
+                employee_total_salary.append(int(e[2]))
+            else:
+                pass
+        data = np.array(employee_total_salary)
+        print "this is the box plot for all employees"
+        plt.boxplot(data)
+        plt.ylabel("Salary (USD)")
+        plt.ylim([0,200000])
+        plt.xlabel("All employees hired by UVM")
+        plt.show()
+
+       # return np_employee_total_salary
+            # print e[2]
+            # break
+
+        # for x in self.dict_of_pos_vs_salaryList:
+        #     all_salary_np.append(x[1])
+        # pprint.pprint(all_salary_np)
+
+
     # def makeBoxPlot(self):
     #     for x in self.dict_Position_listSalaries:
     #         print "This is the box plot for: ", x
@@ -75,20 +111,27 @@ class SchoolYearData:
 
 ######### TEST ############
 e = SchoolYearData(2015, "uvmSalary15.csv")
+# e.makeBoxPlot()  # works
+# e.boxPlotAllEmployees()
+e.boxPlotAllEmployees()
+
 # print "data year of this object: ", e.dataYear
 # print "gets data from this clean file: ", e.cleanFileName
 # print "total count of employees", e.uniquePositionsCount
 # print "get total salaries of all employees at UVM", e.totalSalary
 # print "average salary of UVM employee: ", e.averageSalary()
 # pprint.pprint(e.getAllSalariesPosition())
-dict_of_pos_vs_salaryList = e.getAllSalariesPosition()
 
-for x in dict_of_pos_vs_salaryList:
-    print "this is the box plot for: ", x
-    data = dict_of_pos_vs_salaryList[x]
-    plt.boxplot(data)
-    plt.show()
-    break
+
+# dict_of_pos_vs_salaryList = e.getAllSalariesPosition()
+#
+# for x in dict_of_pos_vs_salaryList:
+#     print "this is the box plot for: ", x
+#     data = dict_of_pos_vs_salaryList[x]
+#     plt.boxplot(data)
+#     plt.ylabel("Salary (USD)")
+#     plt.show()
+#     break
 
     #     for x in self.dict_Position_listSalaries:
     #         print "This is the box plot for: ", x
